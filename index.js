b984eeb4219c5f9d6e167f3abda3196ce06ebe80
TipicultBiomassa <48147673+TipicultBiomassa@users.noreply.github.com>
2022-12-13 01:34:13 +0300

    Add files via upload
    create draggable element

---
+++ index.js
@@ -0,0 +1,56 @@
+const movableDiv = document.getElementById("draggable-div");
+const borderDiv = document.getElementById("border-div");
+
+movableDiv.addEventListener('mousedown',pickup);
+movableDiv.addEventListener('touchstart',pickup);
+
+movableDiv.addEventListener('mousemove',move);
+movableDiv.addEventListener('touchmove',move);
+
+movableDiv.addEventListener('mouseup',drop);
+movableDiv.addEventListener('touchend',drop);
+
+borderDiv.addEventListener('mouseout',drop)
+let draggableElement = null;
+
+function pickup(event) {
+    draggableElement = event.target;
+    draggableElement.style.height = draggableElement.clientHeight;
+    draggableElement.style.width = draggableElement.clientWidth;
+}
+function drop() {
+    if (draggableElement) {
+        draggableElement = null;
+    }
+}
+function move(event) {
+    if (draggableElement) {
+        let leftPrevious = draggableElement.style.left;
+        let topPrevious = draggableElement.style.top;
+        let offsetX, offsetY;
+        if (event.clientX) {
+            offsetX = event.clientX - draggableElement.clientWidth/2;
+            offsetY = event.clientY - draggableElement.clientHeight/2;
+        } else {
+            offsetX =event.changedTouches[0].clientX - draggableElement.clientWidth/2;
+            offsetY =event.changedTouches[0].clientY - draggableElement.clientHeight/2;
+        }
+        draggableElement.style.left = offsetX + 'px';
+        draggableElement.style.top = offsetY + 'px';
+        if(!elementsOverlap(borderDiv, draggableElement)){
+            draggableElement.style.left = leftPrevious;
+            draggableElement.style.top = topPrevious;
+        }
+    }
+}
+function elementsOverlap(el1, el2) {
+    const domRect1 = el1.getBoundingClientRect();
+    const domRect2 = el2.getBoundingClientRect();
+
+    return !(
+        domRect1.top > domRect2.top ||
+        domRect1.right < domRect2.right ||
+        domRect1.bottom < domRect2.bottom ||
+        domRect1.left > domRect2.left
+    );
+}
